eabe42708a8712e943272cf7012aa1467a2ccfde
AlexWalker222 <alexw3071@gmail.com>
2023-09-27 15:27:49 -0400

    ''''

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import "./CSS/App.css";
+import "./App.css";
 
 const audio = document.getElementById("beep");
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./CSS/App.css";
-import "./assets/all.min.css";
-import "./assets/fontawesome.min.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
