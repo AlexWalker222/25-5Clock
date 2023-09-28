4012aa39113801b6a08404c5012d9dc1aa0b6414
AlexWalker222 <alexw3071@gmail.com>
2023-09-27 22:46:46 -0400

    'fixing error'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <React.Component>
     <App />
-  </React.StrictMode>
+  </React.Component>
 );
