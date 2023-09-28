2990accf82dcf6c987003a7b408423f955b142ba
AlexWalker222 <alexw3071@gmail.com>
2023-09-28 16:07:57 -0400

    ''

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from "./App.js";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.Component>
+  <>
     <App />
-  </React.Component>
+  </>
 );
